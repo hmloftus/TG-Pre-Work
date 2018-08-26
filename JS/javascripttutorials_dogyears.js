df480b524e26b72906a6f8214b165202b8dee383
Hannah Loftus <>
2018-08-26 14:40:39 -0600

    JS Variables Dog Years

---
+++ JS/javascripttutorials_dogyears.js
@@ -0,0 +1,27 @@
+//my age in years
+const myAge = 28;
+
+//value will change
+let earlyYears = 2;
+
+//multiplication assignment operator
+earlyYears *= 10.5;
+
+//laterYears is changeable
+let laterYears = myAge - 2;
+
+//laterYears by 4
+laterYears *= 4;
+
+//debug
+console.log(earlyYears, laterYears);
+
+//age in dog years
+const myAgeInDogYears = earlyYears + laterYears;
+
+//my name variable to lowercase
+let myName = "Hannah".toLowerCase();
+console.log(myName);
+
+//my name and my age in dog years
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
