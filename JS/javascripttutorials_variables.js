b36b6e8d15f57657e0b7224066adcb880683355a
Hannah Loftus <>
2018-08-26 13:57:50 -0600

    JS Variables 1.5

--- JS/javascripttutorials_variables.js
+++ JS/javascripttutorials_variables.js
@@ -1,3 +1,13 @@
-let changeMe = true;
-changeMe = false;
-console.log(changeMe);
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule += 16;
+
+// Multiply and assign to particle below
+particle *= 6.02;
+
+// Increment assay by 1
+assay++;
+
