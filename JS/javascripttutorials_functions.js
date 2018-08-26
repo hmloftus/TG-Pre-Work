dfa7e79f39a72cff8fe43a67fb62d8fa6cec20dd
Hannah Loftus <>
2018-08-26 16:08:26 -0600

    JS Functions 1.4

---
+++ JS/javascripttutorials_functions.js
@@ -0,0 +1,9 @@
+const takeOrder = (topping, crustType) => {
+    console.log(`Order: ${crustType} pizza topped with ${topping}`);
+  };
+  
+  takeOrder('mushrooms');
+  
+  takeOrder('thin', 'pineapple');
+  takeOrder('thick', 'cheese');
+  takeOrder('stuffed', 'peppers');
