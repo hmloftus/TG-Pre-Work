cd707893f8a915b3f132d05eda3c2a53ada67ecb
Hannah Loftus <>
2018-08-27 00:51:29 -0600

    JS Loops 1.5

--- JS/javascripttutorials_loops.js
+++ JS/javascripttutorials_loops.js
@@ -1,7 +1,12 @@
-let vacationSpots = ['Spain', 'Peru', 'Chamonix'];
+let myPlaces= ['Peru', 'Chamonix', 'Japan'];
+let friendPlaces= ['Peru', 'France', 'Spain'];
 
-for (let vacationSpotIndex = vacationSpots.length-1; vacationSpotIndex >= 0; vacationSpotIndex--) {
-  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+for (let myPlacesIndex= 0; myPlacesIndex< myPlaces.length; myPlacesIndex++) {
+  for (let friendPlacesIndex=0; friendPlacesIndex< friendPlaces.length; friendPlacesIndex++){
+    if (myPlaces[myPlacesIndex]===friendPlaces[friendPlacesIndex]) {
+      console.log('This is in common');
+    } else {
+      console.log('not in common');
+    }
+  }
 }
-
-
