00ceb9dbd4cbcfca7434c2ca9ed81948c193977e
Hannah Loftus <>
2018-08-26 23:48:37 -0600

    JS Arrays 1.6

--- JS/javascripttutorials_arrays.js
+++ JS/javascripttutorials_arrays.js
@@ -7,3 +7,15 @@ console.log(listItem);
 
 console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
+
+newYearsResolutions[1]= 'Learn a new language';
+console.log(newYearsResolutions);
+
+console.log(newYearsResolutions.length);
+
+newYearsResolutions.push('yoga', 'read');
+console.log(newYearsResolutions);
+
+newYearsResolutions.pop();
+console.log(newYearsResolutions);
+
