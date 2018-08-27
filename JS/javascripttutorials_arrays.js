546adbce1c3b446b326d7095f61665d2946395ce
Hannah Loftus <>
2018-08-26 23:35:03 -0600

    JS Arrays 1.3

---
+++ JS/javascripttutorials_arrays.js
@@ -0,0 +1,9 @@
+const newYearsResolutions = ['eat', 'sleep', 'workout'];
+
+console.log(newYearsResolutions);
+
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
