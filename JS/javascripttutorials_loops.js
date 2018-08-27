ada40aa44fa0ce5e8bd03dde97d1563cb0e3895c
Hannah Loftus <>
2018-08-27 00:39:28 -0600

    JS Loops 1.4

---
+++ JS/javascripttutorials_loops.js
@@ -0,0 +1,7 @@
+let vacationSpots = ['Spain', 'Peru', 'Chamonix'];
+
+for (let vacationSpotIndex = vacationSpots.length-1; vacationSpotIndex >= 0; vacationSpotIndex--) {
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
+
+
