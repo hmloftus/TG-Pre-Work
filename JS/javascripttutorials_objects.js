eb279a56db0c4415b451fc48807e7e338acf6fe7
Hannah Loftus <>
2018-08-27 19:37:15 -0600

    JS Objects 1.14

--- JS/javascripttutorials_objects.js
+++ JS/javascripttutorials_objects.js
@@ -1,44 +1,28 @@
 let person = {
-    name: 'Tyron',
-    age: 40,
-    weekendAlarm: 'No alarms needed',
-    weekAlarm: 'Alarm set to 7AM',
+    _name: 'Lu Xun',
+    _age: 137,
     
-    sayHello: function() {
-      return `Hello, my name is ${this.name}`;
+    set age(ageIn) {
+      if (typeof ageIn === 'number') {
+        this._age = ageIn;
+      }
+      else {
+        console.log('Invalid input');
+        return 'Invalid input';
+      }
     },
     
-    sayGoodbye() {
-      return 'Goodbye!';
+    get age() {
+      console.log(`${this._name} is ${this._age} years old.`);
+      return this._age;
     }
+  
   };
   
   
-  let friend = {
-    name: 'Lebron'
-  };
+  person.age = 'Thirty-nine';
+  person.age = 39;
   
-  friend.sayHello = person.sayHello;
+  console.log(person.age);
   
-  console.log(friend.sayHello());
-  console.log(person.sayHello());
-  
-  person.hobbies = ['Basketball', 'Coaching'];
-  person.hobbies = ['Basketball'];
-  console.log(person.hobbies);
-  
-  
-  console.log(person['name']);
-  console.log(person['age']);
-  
-  let day = 'Tuesday';
-  let alarm;
-  
-  if (day === 'Saturday' || day === 'Sunday' ) {
-    alarm = 'weekendAlarm';
-  } else {
-    alarm = 'weekAlarm';
-  }
-  
-  console.log(person[alarm]);
   
