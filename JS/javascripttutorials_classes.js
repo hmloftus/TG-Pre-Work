fb26c6c75256c5f0d1267ddebaef126c3e683d71
Hannah Loftus <>
2018-08-28 09:14:53 -0600

    JS Classes 1.8

--- JS/javascripttutorials_classes.js
+++ JS/javascripttutorials_classes.js
@@ -1,16 +1,11 @@
-class Surgeon {
-    constructor(name, department) {
+class HospitalEmployee {
+    constructor(name) {
       this._name = name;
-      this._department = department;
       this._remainingVacationDays = 20;
     }
     
     get name() {
       return this._name;
-    }
-    
-    get department() {
-      return this._department;
     }
     
     get remainingVacationDays() {
@@ -22,8 +17,10 @@ class Surgeon {
     }
   }
   
-  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
-  console.log(surgeonCurry.name);
-  surgeonCurry.takeVacationDays(3);
-  console.log(surgeonCurry.remainingVacationDays);
+  class Nurse extends HospitalEmployee {
+    constructor(name,certifications){
+      super(name);
+      this._certifications = certifications;
+    }
+  }
+  
