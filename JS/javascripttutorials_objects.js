c50c4362ae794098aac012f63fb7629995bed4a0
Hannah Loftus <>
2018-08-27 19:32:15 -0600

    JS Objects 1.7

--- JS/javascripttutorials_objects.js
+++ JS/javascripttutorials_objects.js
@@ -5,10 +5,12 @@ let person = {
     weekAlarm: 'Alarm set to 7AM'
   };
   
+  person.hobbies = ['bike', 'hike', 'run']
+  
   console.log(person['name']);
   console.log(person['age']);
   
-  let day = 'Monday';
+  let day = 'Tuesday';
   let alarm;
   
   if (day === 'Saturday' || day === 'Sunday' ) {
@@ -18,26 +20,7 @@ let person = {
   }
   
   console.log(person[alarm]);
+  console.log(person.hobbies)
+  person.hobbies =['run']
+  console.log(person.hobbies)
   
-  let person = {
-    name: 'Hannah',
-    age: 28,
-    weekendAlarm: 'No alarms needed',
-    weekAlarm: 'Alarm set to 7AM'
-  };
-  
-  console.log(person['name']);
-  console.log(person['age']);
-  
-  let day = 'Monday';
-  let alarm;
-  
-  if (day === 'Saturday' || day === 'Sunday' ) {
-    alarm = 'weekendAlarm';
-  } else {
-    alarm = 'weekAlarm';
-  }
-  
-  console.log(person[alarm]);
-  
-    
